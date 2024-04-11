ec9e020a0b2f60d41e8728f6dfd6fbab13575d8a
Trevor Mustoe <trevormustoe0405@gmail.com>
2024-04-11 12:00:46 -0400

    Fixed the wordCounter function to properly split the input to an array and return the array length as an html element.

--- main.js
+++ main.js
@@ -2,14 +2,18 @@ console.log("Connected!")
 // YOU WILL MODIFY THIS FUNCTION TO GET THE PROGRAM TO WORK
 const wordCounter = (value) => {
   // Type into the input and press submit, you will see the value you entered in your console
-  console.log(value);
+  console.log(value.split(" "));
+
+// Need to split the strings inputed and add them to an array. 
+// Count the array.length and return the array length. 
 
   if (value) {
-    // COMPLETE THE LOGIC 
-    wordCount.innerHTML = `Word Count: 0`; 
+    // COMPLETE THE LOGIC
+    let newArray = value.split(" ") 
+    wordCount.innerHTML = `Word Count: ${newArray.length}`; 
   } else {
     // if the value is empty, set the error message value to "Please input text"
-    error.innerHTML = ""; // UPDATE THIS
+    error.innerHTML = "Please input text"; // UPDATE THIS
   }
 }
 
